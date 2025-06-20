4dc6928ac4f0a3cfc25776bbd1b3ddd9961d4bd2
Emmanuel Kiprotich <164993155+emmanuelKiprotich@users.noreply.github.com>
2025-06-20 09:22:29 +0300

    bubble_sort/bubble_sort.py

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,8 @@
 def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+    arr = list(unsorted_list)
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
